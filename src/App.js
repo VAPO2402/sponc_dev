5ea0948d914aa1c95ff4cfbaa6f0444549de287e
Pratyush Kumar <pratyushkumardelhi55@gmail.com>
2024-07-18 14:07:51 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
     </>
   );
 }
