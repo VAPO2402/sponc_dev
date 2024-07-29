1bf068963a23e36c01fd25841e43344e774edeba
Pratyush Kumar <pratyushkumardelhi55@gmail.com>
2024-07-29 09:31:36 +0530

    again

--- src/components/Home/Landing.jsx
+++ src/components/Home/Landing.jsx
@@ -1,37 +1,8 @@
 import React from "react";
 
-function Card(props){
-  return (
-    <>
-    </>
-  );
-}
-
 const Landing = () => {
   return (
     <>
-      <div className="main">
-        <div className="landingMain">
-          <div className="landleft"></div>
-          <div className="landMiddle"></div>
-          <div className="landRight"></div>
-        </div>
-        <div className="landingBottom">
-          <div className="landBl">
-            <Card />
-            <Card />
-            <Card />  
-            <Card />
-          </div>
-          <div className="landBm"></div>
-          <div className="landBr">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-          </div>
-        </div>
-      </div>
     </>
   );
 };
